caf3e1f20f51cc71e990eaa76a8f47c09e9da050
haya0002 <rahim.hayat@etudiant.univ-reims.fr>
2022-04-24 19:29:09 +0200

    Fix bug + save colors
    - fixed a bug after a clear (trash) then a redo.
    
    - New : Colors are now saved after performing actions (undo, redo, clear)

--- DrawIT_App/DrawIT.py
+++ DrawIT_App/DrawIT.py
@@ -46,9 +46,12 @@ dashboard.place(x = 25, y = 75)
 # Methode pour dessiner sur le dashboard avec <B1-Motion>
 def draw(event):
     global init_x, init_y
-    global selectLine
+    global selectLine, poubelle_cleaned, gomme
+    #if poubelle_cleaned:
+     #   selectLine+=1
+      #  poubelle_cleaned = False
     if init_x and init_y:
-        dashboard.create_line(init_x,init_y,event.x,event.y,width=pen_width,fill=pen_color,capstyle=ROUND,smooth=True,tag="line"+str(selectLine))
+        dashboard.create_line(init_x,init_y,event.x,event.y,width=pen_width,fill=pen_color,capstyle=ROUND,smooth=True,tag=("gomme" if gomme==True else "line"+str(selectLine)))
 
     init_x = event.x
     init_y = event.y
@@ -58,60 +61,81 @@ def reset(event):
     init_x = None
     init_y = None
     selectLine +=1
-    redo_list.clear() # si un nouveau trait est dessiné, 
+    # suppression des couleurs des lignes de redo_list
+    if len(redo_list) > 0 and len(poubelle_list_item) == 0 :
+        for i in range(len(redo_list)):
+            del line_color[0]
+    redo_list.clear() # si un nouveau trait est dessiné
 
 def clear():
-    global selectLine
+    global selectLine, poubelle_cleaned
     deleted_line = selectLine
     poubelle_list = [] # Liste des lignes supprimées avec la poubelle
+    color_list = [] # Liste des couleurs des lignes supprimées avec la poubelle
     for deleted_line in range(deleted_line):
         for item in dashboard.find_withtag("line"+str(deleted_line)):
             poubelle_list.append(dashboard.coords(item))
+            color_list.append(dashboard.itemcget(item, "fill"))
         poubelle_list_item.append(poubelle_list.copy())
+        line_color.append(color_list[-1])
+        # suppression
         poubelle_list.clear()
-    selectLine += 1 # On peux récupérer le contenue de la poubelle
+        color_list.clear()
+    
     redo_list.clear() # On ne peux plus rétablir car nous avons effectué l'action de suppression
     dashboard.delete(ALL) # Efface le dashboard
+    poubelle_cleaned = True
 
 def change_pen_color(eraser=False, color="multicolor"):  # pinceau/gomme
-    global pen_color, pen_color_tmp
+    global pen_color, pen_color_tmp, gomme
     if eraser:
+        gomme = True
         pen_color_tmp = pen_color
         pen_color = dashboard_color
     elif color == "multicolor":
         pen_color=colorchooser.askcolor(color=pen_color)[1]
+        gomme = False
     else:
         pen_color = color
+        gomme = False
 
 def change_dashboard_color():  # Change la couleur du dashboard
-    global dashboard_color
+    global dashboard_color    
     dashboard_color = colorchooser.askcolor(color=dashboard_color)[1]
     dashboard.configure(bg = dashboard_color)
+    for item in dashboard.find_withtag("gomme"):
+        dashboard.itemconfig(item, fill=dashboard_color)
 
 def change_brush_width(event):  # Change la taille du pinceau à l'aide d'un slider
     global pen_width
     pen_width = event
-    
+
 def undo():
     global selectLine
     if len(poubelle_list_item) > 0:
         item_selected = 0
         for item in poubelle_list_item: # récupérer toute les lignes avec le même tag en 1 item
             for coords in item:
-                dashboard.create_line(coords,width=pen_width,fill=pen_color,capstyle=ROUND,smooth=True, tag="line"+str(item_selected))
+                dashboard.create_line(coords,width=pen_width,fill=line_color[0],capstyle=ROUND,smooth=True, tag="line"+str(item_selected))
             item_selected += 1
-        selectLine -= 1
+            # supprimer la première case de line_color
+            del line_color[0]
+        #selectLine -= 1
         poubelle_list_item.clear()
     elif selectLine > 0:
+        coords_list = [] # Coordonnées des lignes supprimées avec le même tag
+        color_list = [] # Couleur des lignes supprimées avec la même couleur
         for item in dashboard.find_withtag("line"+str(selectLine-1)): # récupérer toute les lignes avec le même tag en 1 item
             if dashboard.find_withtag("line"+str(selectLine-1)): # si c'est bien la dernière ligne
                 coords_list.append(dashboard.coords(item))
+                color_list.append(dashboard.itemcget(item, "fill"))
         redo_list.append(coords_list.copy()) # on fait une copie des coordonnées de l'item
+        line_color.append(color_list[0]) # on récupère la couleur de la ligne
+
         coords_list.clear() # on peux donc supprimer ceci car on a fait au préalable une copie
+        color_list.clear()
         dashboard.delete("line"+str(selectLine-1))
         selectLine -= 1
-        #print("Debug UNDO :")
-        #print(redo_list)
     else:
         print("Aucune ligne à supprimer")
 
@@ -120,17 +144,28 @@ def redo():
     if len(redo_list) > 0:
         item = redo_list[-1] # on récupère le dernier item
         for coords in item: # on récupère les coordonnées de cette item
-            dashboard.create_line(coords,width=pen_width,fill=pen_color,capstyle=ROUND,smooth=True, tag="line"+str(selectLine))
+            dashboard.create_line(coords,width=pen_width,fill=line_color[-1],capstyle=ROUND,smooth=True, tag="line"+str(selectLine))
         selectLine += 1
         redo_list.pop()
+        line_color.pop()
     else:
         print("Aucune ligne à rétablir")
 
 
+def debug():
+    print("———————————————————— Debug : ————————————————————")
+    print("line ID : "+str(selectLine))
+    print(f"Debug line_color [{len(line_color)}]: ")
+    print(line_color)
+    print(f"Debug redo_list [{len(redo_list)}] : ")
+    print(redo_list)
+
 redo_list = [] # Liste des lignes supprimées
-coords_list = [] # Coordonnées des lignes supprimées avec le même tag
 poubelle_list_item = [] # Liste des items supprimés avec la poubelle (par item)
+line_color = [] # Couleur du pinceau pour chaque dessin effectué
 selectLine = 0
+poubelle_cleaned = False
+gomme = False
 # Dessine un trait du pinceau jusqu'au relachement du clic de la souris
 dashboard.bind("<B1-Motion>", draw)
 dashboard.bind('<ButtonRelease-1>', reset)
